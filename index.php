170867856bab6a395e81aa49d08864a0a292d5e9
khoirur-rofi <khoirurrofi146@gmail.com>
2026-08-13 18:42:28 +0700

    layout hp 3

--- index.php
+++ index.php
@@ -792,7 +792,7 @@ $isSuratTidakMampuOpen = in_array($page, $suratTidakMampuPages, true);
             border-color: rgba(255, 255, 255, 0.06);
         }
     </style>
-    <link href="css/responsive.css" rel="stylesheet" />
+    <link href="css/responsive.css?v=<?= filemtime(__DIR__ . '/css/responsive.css') ?>" rel="stylesheet" />
 </head>
 
 <body class="sb-nav-fixed">
